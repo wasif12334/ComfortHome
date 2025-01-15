705202a3a30181c875ff6f734737895b1b6b9a62
wasif12334 <kashifaliraza95@gmail.com>
2025-01-15 17:22:48 +0500

    changes the layout of rooms-section and added the new page which dynimicaly load the content form home page to room-lage

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-document.addEventListener('DOMContentLoaded', () => {
-  let availBtn = document.querySelector('.form-btn')
 
-  let totalRooms = 10;
-  let avail =true;
-  availBtn.addEventListener('click',()=>{
+let btn =document.querySelectorAll('.BookNow-btn');
+
+btn.forEach((btn)=>{
+  btn.addEventListener('click',()=>{
+    let roomId = btn.getAttribute("data-id");
   
-    const adults = document.querySelector('.adults')
-    const checkIn = document.querySelector('.checkindate').value
-    const checkOut = document.querySelector('.checkioutdate')
-    const userRooms = parseInt(document.querySelector('.numberofrooms').value);
-    console.log(adults.value,checkIn,checkOut,userRooms);
+  
+    window.location.href = `room-details.html?roomId=${roomId}`;
   })
-});
+  
+})
