c99993b379a2b108006376f7817e654964e7c7c8
wasif12334 <kashifaliraza95@gmail.com>
2025-01-15 16:01:08 +0500

    changes the rooms content

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-let availBtn = document.querySelector('.form-btn')
+document.addEventListener('DOMContentLoaded', () => {
+  let availBtn = document.querySelector('.form-btn')
 
-let totalRooms = 10;
-let avail =true;
-availBtn.addEventListener('click',()=>{
-  const adults = document.querySelector('.adults').value;
-  const checkIn = document.querySelector('.checkindate').value;
-  const checkOut = document.querySelector('.checkioutdate').value;
-  const userRooms = document.querySelector('.numberofrooms').value;
-  console.log(adults,checkIn,checkOut,userRooms);
-})
+  let totalRooms = 10;
+  let avail =true;
+  availBtn.addEventListener('click',()=>{
+  
+    const adults = document.querySelector('.adults')
+    const checkIn = document.querySelector('.checkindate').value
+    const checkOut = document.querySelector('.checkioutdate')
+    const userRooms = parseInt(document.querySelector('.numberofrooms').value);
+    console.log(adults.value,checkIn,checkOut,userRooms);
+  })
+});
